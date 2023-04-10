9674c9b70d0605490d2cb5e2eb56818e6c682cd5
Litvinov Yuriy <litvinov.yuriy@gmail.com>
2023-04-10 11:37:44 +0400

    fix engine hbs

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   app.useStaticAssets(resolve('./src/public'));
   app.setBaseViewsDir(resolve('./src/views'));
-  app.setViewEngine('pug');
+  app.setViewEngine('hbs');
 
   await app.listen(process.env.PORT);
 }
