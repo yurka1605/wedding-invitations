ff93eb257289cafad58a4b401d7fa425da4aa6c5
Litvinov Yuriy <litvinov.yuriy@gmail.com>
2023-04-12 18:45:10 +0400

    added method for work with links

--- src/google-sheets.service.ts
+++ src/google-sheets.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { GoogleSpreadsheet, GoogleSpreadsheetRow } from 'google-spreadsheet';
+import { GoogleSpreadsheet, GoogleSpreadsheetRow, GoogleSpreadsheetWorksheet } from 'google-spreadsheet';
 import * as creds from '../client_secret.json';
 import { AnswerDto } from './answer.dto';
 
 @Injectable()
 export class GoogleSheetsService {
-
   private doc: GoogleSpreadsheet;
   private sheetId = '1mVLNduuKlWfjtvVY_EHJrYQ-T8ETpS4NdQMDdburuso';
+  private sheetIndex = 0;
   private data: GoogleSpreadsheetRow[];
+  private link = 'https://wedding-invitation-phi-blush.vercel.app/?invitations=';
+  private rowLinkIndex = 4;
+
+  get sheet(): GoogleSpreadsheetWorksheet {
+    return this.doc.sheetsByIndex[this.sheetIndex];
+  }
 
   constructor() {
     this.init();
@@ -16,19 +22,22 @@ export class GoogleSheetsService {
 
   async refreshRowData() {
     await this.doc.loadInfo();
-    this.data = await this.doc.sheetsByIndex[0].getRows();
+    this.data = await this.sheet.getRows();
   }  
 
   async init(): Promise<void> {
     this.doc = new GoogleSpreadsheet(this.sheetId);
     await this.doc.useServiceAccountAuth(creds);
     await this.refreshRowData();
+    
+    // uncommented if need update links
+    // this.updateLinksForUsers();
   }
 
   async getUsersNamesByIds(ids: number[]) {
     await this.refreshRowData();
     return ids.map(id => {
-      const invited = this.data[id - 1];
+      const invited = this.data[id - 2];
       return invited.alias;
     });
   }
@@ -37,7 +46,7 @@ export class GoogleSheetsService {
   async updateViews(ids: number[]) {
     await this.refreshRowData();
     ids.forEach(id => {
-      const invited = this.data[id - 1];
+      const invited = this.data[id - 2];
       invited.viewCount++;
       invited.save();
     });
@@ -46,9 +55,19 @@ export class GoogleSheetsService {
   async setAnswer(ids: number[], answerDto: AnswerDto) {
     await this.refreshRowData();
     ids.forEach(id => {
-      const invited = this.data[id - 1];
+      const invited = this.data[id - 2];
       invited.status  = answerDto.answer ? 'Yes' : 'No';
       invited.save();
     });
+  }
+
+  private async updateLinksForUsers() {
+    await this.sheet.loadCells();
+    this.data.forEach(el => {
+      el.link = this.link + el.rowIndex;
+      this.sheet.getCell(el.rowIndex - 1, this.rowLinkIndex).value = el.link;
+    });
+
+    this.sheet.saveUpdatedCells();
   }
 }
